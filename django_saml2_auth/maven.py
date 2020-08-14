8f91a1e8116189eac4818b29b60e727ecbd3011f
dgonzo27 <dylan.gonzales@metisgenetics.com>
2020-08-14 15:46:18 -0500

    signin url redirects

--- django_saml2_auth/maven.py
+++ django_saml2_auth/maven.py
@@ -128,12 +128,7 @@ def signin(r):
     logger.debug('maven.signin')
     saml_client = get_saml_client(get_current_domain(r))
     _, info = saml_client.prepare_for_authenticate()
-    redirect_url = None
-
-    for key, value in info['headers']:
-        if key == 'Location':
-            redirect_url = value
-            break
+    redirect_url = settings.MAVEN_SAML2_AUTH.get('METADATA_AUTO_CONF_URL')
 
     return HttpResponseRedirect(redirect_url)
 
