1dcfe326d402894f7dc92c78d7dc951dfff1d9d4
dgonzo27 <dylan.gonzales@metisgenetics.com>
2020-08-14 16:43:45 -0500

    updates

--- django_saml2_auth/maven.py
+++ django_saml2_auth/maven.py
@@ -128,7 +128,7 @@ def signin(r):
     logger.debug('maven.signin')
     saml_client = get_saml_client(get_current_domain(r))
     _, info = saml_client.prepare_for_authenticate()
-    redirect_url = settings.MAVEN_SAML2_AUTH.get('METADATA_AUTO_CONF_URL')
+    redirect_url = str(settings.MAVEN_SAML2_AUTH['METADATA_AUTO_CONF_URL'])
 
     return HttpResponseRedirect(redirect_url)
 
