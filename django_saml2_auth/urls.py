01391ef23738d273d293512a142d2bbb90dc3ea0
andrewKilburn <106087215+andrewKilburn@users.noreply.github.com>
2022-08-22 16:08:48 -0500

    Update for Django 4.0

--- django_saml2_auth/urls.py
+++ django_saml2_auth/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views, maven, tch
 
 app_name = 'django_saml2_auth'
 
 urlpatterns = [
-    url(r'^maven_acs/$', maven.acs, name="maven_acs"),
-    url(r'^tch_acs/$', tch.acs, name="tch_acs"),
-    url(r'^denied/$', views.denied, name="denied"),
+    re_path(r'^maven_acs/$', maven.acs, name="maven_acs"),
+    re_path(r'^tch_acs/$', tch.acs, name="tch_acs"),
+    re_path(r'^denied/$', views.denied, name="denied"),
 ]
